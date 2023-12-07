da594bbf2540b1181f18aed923ce047a9f0e5dd0
Daniel Schild <daniel.f.schild@googlemail.com>
2023-12-07 20:03:14 +0100

    Upgrade output

--- aoc2023.go
+++ aoc2023.go
@@ -10,15 +10,17 @@ import (
 	"daniel/aoc2023/day6"
 	"daniel/aoc2023/day7"
 	"fmt"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const Day = 0
 
 func main() {
-	fmt.Println("AOC 2023")
 	localDay := Day
 	if localDay == -1 {
 		fmt.Println("Please select the day:")
@@ -46,7 +48,6 @@ func main() {
 		runDay5()
 		runDay6()
 		runDay7()
-		fmt.Println("###############################")
 	case 1:
 		runDay1()
 	case 2:
@@ -64,103 +65,80 @@ func main() {
 	default:
 		fmt.Println("Day does not exist")
 	}
-
+	printRuns()
 }
 
 func runDay1() {
-	fmt.Println("######### Day 1 Ex 1 ##########")
-	out := day1.N1("day1/test1.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day1.N1("day1/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
-
-	fmt.Println("######### Day 1 Ex 2 ##########")
-	out = day1.N2("day1/test2.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day1.N2("day1/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
+	runAndPrint(day1.N1, []string{"day1/test1.txt", "day1/input.txt"}, "1", "1")
+	runAndPrint(day1.N2, []string{"day1/test2.txt", "day1/input.txt"}, "1", "2")
 }
 
 func runDay2() {
-	fmt.Println("######### Day 2 Ex 1 ##########")
-	out := day2.N1("day2/test1.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day2.N1("day2/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
-
-	fmt.Println("######### Day 2 Ex 2 ##########")
-	out = day2.N2("day2/test2.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day2.N2("day2/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
+	runAndPrint(day2.N1, []string{"day2/test1.txt", "day2/input.txt"}, "2", "1")
+	runAndPrint(day2.N2, []string{"day2/test2.txt", "day2/input.txt"}, "2", "2")
 }
 
 func runDay3() {
-	fmt.Println("######### Day 3 Ex 1 ##########")
-	out := day3.N1("day3/test1.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day3.N1("day3/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
-
-	fmt.Println("######### Day 3 Ex 2 ##########")
-	out = day3.N2("day3/test2.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day3.N2("day3/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
+	runAndPrint(day3.N1, []string{"day3/test1.txt", "day3/input.txt"}, "3", "1")
+	runAndPrint(day3.N2, []string{"day3/test2.txt", "day3/input.txt"}, "3", "2")
 }
 
 func runDay4() {
-	fmt.Println("######### Day 4 Ex 1 ##########")
-	out := day4.N1("day4/test1.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day4.N1("day4/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
-
-	fmt.Println("######### Day 4 Ex 2 ##########")
-	out = day4.N2("day4/test2.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day4.N2("day4/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
+	runAndPrint(day4.N1, []string{"day4/test1.txt", "day4/input.txt"}, "4", "1")
+	runAndPrint(day4.N2, []string{"day4/test2.txt", "day4/input.txt"}, "4", "2")
 }
 
 func runDay5() {
-	fmt.Println("######### Day 5 Ex 1 ##########")
-	out := day5.N1("day5/test.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day5.N1("day5/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
-
-	fmt.Println("######### Day 5 Ex 2 ##########")
-	out = day5.N2("day5/test.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day5.N2("day5/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
+	runAndPrint(day5.N1, []string{"day5/test.txt", "day5/input.txt"}, "5", "1")
+	runAndPrint(day5.N2, []string{"day5/test.txt", "day5/input.txt"}, "5", "2")
 }
 
 func runDay6() {
-	fmt.Println("######### Day 6 Ex 1 ##########")
-	out := day6.N1("day6/test.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day6.N1("day6/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
-
-	fmt.Println("######### Day 6 Ex 2 ##########")
-	out = day6.N2("day6/test.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day6.N2("day6/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
+	runAndPrint(day6.N1, []string{"day6/test.txt", "day6/input.txt"}, "6", "1")
+	runAndPrint(day6.N2, []string{"day6/test.txt", "day6/input.txt"}, "6", "2")
 }
 
 func runDay7() {
-	fmt.Println("######### Day 7 Ex 1 ##########")
-	out := day7.N1("day7/test.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day7.N1("day7/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
+	runAndPrint(day7.N1, []string{"day7/test.txt", "day7/input.txt"}, "7", "1")
+	runAndPrint(day7.N2, []string{"day7/test.txt", "day7/input.txt"}, "7", "2")
+}
 
-	fmt.Println("######### Day 7 Ex 2 ##########")
-	out = day7.N2("day7/test.txt")
-	fmt.Println("--> Solution for Test: ", out)
-	out = day7.N2("day7/input.txt")
-	fmt.Println("--> Solution for Input: ", out)
+type run struct {
+	day      string
+	ex       string
+	input    string
+	result   string
+	duration time.Duration
+}
+
+var runs []run
+
+func runAndPrint(f func(string) string, params []string, day string, ex string) {
+	for _, p := range params {
+		start := time.Now()
+		out := f(p)
+		elapsed := time.Since(start)
+		runs = append(runs, run{
+			day,
+			ex,
+			p,
+			out,
+			elapsed,
+		})
+	}
+}
+
+func printRuns() {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.SetTitle("Advent of Code")
+	t.AppendHeader(table.Row{"Day", "Part", "Input", "Result", "Duration"})
+	for _, r := range runs {
+		t.AppendRows([]table.Row{
+			{r.day, r.ex, r.input, r.result, r.duration},
+		})
+	}
+	t.SetStyle(table.StyleColoredDark)
+	t.Style().Title.Align = text.AlignCenter
+	t.Render()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module daniel/aoc2023
 
 go 1.21
+
+require (
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.4.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
